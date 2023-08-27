bfdda7cf9e357c4daad1bfd992634cee01985d87
김경환 <kh.kim9700@gmail.com>
2023-08-27 12:52:09 +0900

    5번문제

---
+++ 005_Remain1.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution
+{
+    public int solution(int n)
+    {
+        for (int i = 2; i < n; i++)
+        {
+            if ((n - 1) % i == 0)
+                return i;
+        }
+
+        return -1;
+    }
+}
